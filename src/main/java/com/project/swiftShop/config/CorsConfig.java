988ab54696796822943187ee2bcf64764fb8b5f6
p-i-y-u-s-h-r-a-j <piyush06raj@gmail.com>
2023-12-31 19:25:00 +0530

    added CORS origin and CORS Config

---
+++ src/main/java/com/project/swiftShop/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.project.swiftShop.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("api/v1/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)  // If using credentials (e.g., cookies)
+                .maxAge(3600);  // Max age for preflight requests in seconds
+    }
+}
+
