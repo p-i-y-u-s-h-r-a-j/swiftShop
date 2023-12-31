46a2c6a358c6d8fb02a56fedff93f91a9f1c34ec
p-i-y-u-s-h-r-a-j <piyush06raj@gmail.com>
2023-12-31 11:17:50 +0530

    changed Base api

--- src/main/java/com/project/swiftShop/controller/ProductController.java
+++ src/main/java/com/project/swiftShop/controller/ProductController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/v1/product")
 public class ProductController {
 
     @Autowired
--- src/main/java/com/project/swiftShop/controller/WishlistController.java
+++ src/main/java/com/project/swiftShop/controller/WishlistController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/wishlist")
+@RequestMapping("/api/v1/wishlist")
 public class WishlistController {
 
     @Autowired
